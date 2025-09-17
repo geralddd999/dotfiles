10a0e185fc9bc0e9c7fd331045d4297803f5c7de
Gerald Chambi <101535693+geralddd999@users.noreply.github.com>
2025-09-17 21:10:31 +0200

    Update configuration.nix

--- nix-config/configuration.nix
+++ nix-config/configuration.nix
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./nvidia.nix
       ./auto-cpufreq.nix
     ];
 
@@ -21,7 +20,43 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
+  # Machine specific packages
+  environment.systemPackages = with pkgs; [
+    radeontop # Like htop, but for AMD GPUs
+    nvtopPackages.amd # nvtop for AMD GPUs
+  ];
+
+  # Load amdgpu at stage 1
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.kernelParams = [
+    # Force use of the thinkpad_acpi driver for backlight control.
+    # This allows the backlight save/load systemd service to work.
+    "acpi_backlight=native"
+
+    # AMD CPU scaling
+    # https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
+    # https://wiki.archlinux.org/title/CPU_frequency_scaling#amd_pstate
+    # On recent AMD CPUs this can be more energy efficient.
+    "amd_pstate=guided"
+
+    # Load amdgpu at stage 1
+    "amdgpu"
+
+    # With BIOS version 1.12 and the IOMMU enabled, the amdgpu driver
+    # either crashes or is not able to attach to the GPU depending on
+    # the kernel version. I've seen no issues with the IOMMU disabled.
+    #
+    # BIOS version 1.13 fixes the IOMMU issues, but we leave the IOMMU
+    # in software mode to avoid a sad experience for those people that drew
+    # the short straw when they bought their laptop.
+    #
+    # Do not set iommu=off, because this will cause the SD-Card reader
+    # driver to kernel panic.
+    "iommu=soft"
+  ];  
+
+
+# Set your time zone.
   time.timeZone = "Europe/Paris";
 
   # Configure network proxy if necessary
@@ -49,18 +84,32 @@ hardware.graphics = {
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
-	intel-media-driver
-	#with pkgs.pkgsi686Linux; intel-vaapi-driver #32-bit support intel driver
-	intel-vaapi-driver
-	libvdpau-va-gl
-	mesa libva-utils vulkan-tools
+      # VA-API and VDPAU
+      vaapiVdpau
 
+      # AMD ROCm OpenCL runtime
+      rocmPackages.clr
+      rocmPackages.clr.icd
+
+      # AMDVLK drivers can be used in addition to the Mesa RADV drivers.
+      amdvlk
     ];
+    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
 };
 
-environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "iHD";
+environment.variables = {
+    # VAAPI and VDPAU config for accelerated video.
+    # See https://wiki.archlinux.org/index.php/Hardware_video_acceleration
+    "VDPAU_DRIVER" = "radeonsi";
+    "LIBVA_DRIVER_NAME" = "radeonsi";
 };
+
+#HIP libraries workaround fix;
+
+systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+
 # Hyprland config
   programs.hyprland = {
    enable = true;
